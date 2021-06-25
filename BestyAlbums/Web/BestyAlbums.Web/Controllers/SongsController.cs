0bb47fab0951c591dccbc76da2b7cd1b3f6f567e
emrrar <avatareigan@gmail.com>
2021-06-25 15:10:58 +0300

    Added the rest of the controllers and some minor changes to layout file.

---
+++ BestyAlbums/Web/BestyAlbums.Web/Controllers/SongsController.cs
@@ -0,0 +1,12 @@
+﻿namespace BestyAlbums.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SongsController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
